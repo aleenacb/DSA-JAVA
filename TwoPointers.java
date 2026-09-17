1707cd81633df9c36049a4a01ca5a0f66aa70e43
TechWithAleenaCB <184249015+aleenacb@users.noreply.github.com>
2026-09-17 12:35:46 +0530

    Add files via upload

---
+++ TwoPointers.java
@@ -0,0 +1,21 @@
+package DSA;
+
+//Two Pointer sum using for loop
+public class TwoPointers {
+    public static int[] TwoSum(int[] arr, int target) {
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = i + 1; j < arr.length; j++) {
+                if(arr[i] + arr[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+    public static void main(String[] args) {
+        int[] arr = {2, 7, 11, 15};
+        int target = 18;
+        int[] result = TwoSum(arr, target);
+        System.out.println("Indexes : [" + result[0] + ", " + result[1]+ "]");
+    }
+}
